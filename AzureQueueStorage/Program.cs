132c6bd1d100a97897335e16c7f02c81e172864b
Ram Rajendran <ramrajendran2@gmail.com>
2020-04-23 08:49:55 +0000

    Queuecommit

---
+++ AzureQueueStorage/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.Azure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.Azure.Storage.Queue; // Namespace for Queue storage types
+
+namespace AzureQueueStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            // obj.CreateAQueue();
+           // obj.DisplayMessage();
+           // obj.UpdateContent();
+            //obj.DeQueue();
+
+
+        }
+        
+        public void CreateAQueue()
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a container.
+            CloudQueue queue = queueClient.GetQueueReference("msgqueue");
+
+            // Create the queue if it doesn't already exist
+            queue.CreateIfNotExists();
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage("Food Item 1");
+            queue.AddMessage(message);
+        }
+        public void DisplayMessage()
+        {
+            // Retrieve storage account from connection string
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+            CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue
+            CloudQueue queue = queueClient.GetQueueReference("msgqueue");
+
+            // Peek at the next message
+            CloudQueueMessage peekedMessage = queue.PeekMessage();
+
+            // Display message.
+            Console.WriteLine(peekedMessage.AsString);
+            Console.ReadLine();
+        }
+        public void UpdateContent()
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            CloudQueue queue = queueClient.GetQueueReference("msgqueue");
+
+            // Get the message from the queue and update the message contents.
+            CloudQueueMessage message = queue.GetMessage();
+            message.SetMessageContent2("food Ready.", false);
+            queue.UpdateMessage(message,
+                TimeSpan.FromSeconds(30.0),  // Make it invisible for another 60 seconds.
+                MessageUpdateFields.Content | MessageUpdateFields.Visibility);
+        }
+        public void DeQueue()
+        {
+            // Retrieve storage account from connection string
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue
+            CloudQueue queue = queueClient.GetQueueReference("msgqueue");
+
+            // Get the next message
+            CloudQueueMessage retrievedMessage = queue.GetMessage();
+
+            //Process the message in less than 30 seconds, and then delete the message
+            queue.DeleteMessage(retrievedMessage);
+        }
+
+
+
+    }
+
+}
+
